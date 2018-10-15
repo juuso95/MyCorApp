477f083a7f0256f6793457c3b7ba4b5157cfacac
juuso95 <juuso.kivisto@outlook.com>
2018-10-15 19:19:58 +0300

    TaskList

--- MyCorApp/www/scripts/index.js
+++ MyCorApp/www/scripts/index.js
@@ -5,20 +5,66 @@
 (function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+        console.log(cordova.file);
         
+
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
         var parentElement = document.getElementById('deviceready');
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+    
+
+        
+       //var options = { frequency: 100 };
+       //var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
     };
+   
+
+
+   // var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
+   // function onSuccess(acceleration) {
+     //   x1 = acceleration.x;
+      //  y1 = acceleration.y;
+      //  z1 = acceleration.z;
+
+    //////    var element = document.getElementById('accelerometer');
+     //////   element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+       //////     'Acceleration Y: ' + acceleration.y + '<br />' +
+         //////   'Acceleration Z: ' + acceleration.z + '<br />' +
+          //////  'Timestamp: ' + acceleration.timestamp + '<br />';
+    
+
+       // var td = document.getElementById("list");
+       // td.removeChild(td.childNodes[1]);
+
+  //  }
+  //  function onError(e) {
+        //do something
+   // }
+  // setInterval(function () {
+    //   var change = Math.abs(x1 - x2 + y1 - y2 + z1 - z2);
+
+      //  if (change > 25) {
+
+            //var bodyElement = document.querySelector("body");
+            //bodyElement.style.backgroundColor = getRandomColor();
+
+       //     var td = document.getElementById("list");
+         //   td.removeChild(td.childNodes[1]);
+
+     //  }
+     //  x2 = x1;
+     //  y2 = y1;
+     //  z2 = z1;
+   // }, 100);
 
     function onPause() {
         // TODO: This application has been suspended. Save application state here.
@@ -27,4 +73,5 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
+
 } )();
---
+++ MyCorApp/www/scripts/script.js
@@ -0,0 +1,40 @@
+﻿
+    //TIME
+    var d = new Date();
+    document.getElementById("date").innerHTML = d.toDateString();
+
+    //ADDING TASK with TIME
+    function myFunction() {
+
+        var td = document.createElement("TD");
+        var tr = document.createElement("TR");
+        td.setAttribute('id', 'td');
+        tr.setAttribute('id', 'tr');
+
+        var input = document.getElementById("myText");
+        td.innerHTML = "<b>" + input.value + "</b>" + " | " + d.toDateString();
+        input.value = "";
+
+        document.getElementById("list").appendChild(tr).appendChild(td);
+
+
+    }
+
+    //REMOVING TASKS
+    function myFunction1() {
+
+        try {
+
+            var td = document.getElementById("object");
+            td.parentNode.removeChild(td);
+
+        }
+
+        catch (err) {
+
+            var td = document.getElementById('td', 'tr');
+            td.parentNode.removeChild(td);
+
+
+        }
+    }
